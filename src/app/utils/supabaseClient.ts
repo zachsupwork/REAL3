42a8d25261b044d075f9a1425d191a5454e511b8
zachsupwork <zachsupwork@gmail.com>
2025-02-15 09:43:00 -0500

    Update supabaseClient.ts

--- src/app/utils/supabaseClient.ts
+++ src/app/utils/supabaseClient.ts
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export function createSupabaseClient() {
     return createClient(
-            process.env.SUPABASE_URL!,
-                    process.env.SUPABASE_SERVICE_KEY!
-                        );
-                        }
-                        
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_SERVICE_KEY!
+    );
+}
